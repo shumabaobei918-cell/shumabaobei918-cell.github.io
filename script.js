7ba1856eb3835824db261c816900ca2101ceece4
shumabaobei918-cell <shumabaobei918@gmail.com>
2025-08-18 21:16:22 +0800

    Update script.js

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ noButton.addEventListener("click", function () {
   }
 
   // 图片变化（前 5 次变化）
-  if (clickCount === 1) mainImage.src = "images/maomisorry.jpg"; // 震惊
+  if (clickCount === 1) mainImage.src = "images/shuairen.gif"; // 震惊
   if (clickCount === 2) mainImage.src = "images/baituo.jpg"; // 思考
   if (clickCount === 3) mainImage.src = "images/wcuole.gif"; // 生气
   if (clickCount === 4) mainImage.src = "images/zaiyebuganle.jpg"; // 哭
@@ -79,6 +79,7 @@ yesButton.addEventListener("click", function () {
   document.body.style.overflow = "hidden";
 
 });
+
 
 
 
